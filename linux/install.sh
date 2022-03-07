6f5f674513a963fa050d9bb5aa62025eca2f8332
Bart Zonneveld <bart.zonneveld@shopify.com>
2022-03-07 12:00:16 +0100

    Added cmd tests for linux

--- linux/install.sh
+++ linux/install.sh
@@ -1,6 +1,16 @@
-sudo apt-get install -y bat
-sudo apt-get install -y exa
-sudo apt-get install -y ripgrep
+if ! command -v bat &> /dev/null; then
+  sudo apt-get install -y bat
+fi
 
-npm i -g diff-so-fancy
+if ! command -v exa &> /dev/null; then
+  sudo apt-get install -y exa
+fi
+
+if ! command -v ripgrep &> /dev/null; then
+  sudo apt-get install -y ripgrep
+fi
+
+if ! command -v diff-so-fancy &> /dev/null; then
+  npm i -g diff-so-fancy
+fi
 
