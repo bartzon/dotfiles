8f0aba5a9fa186da57cf33a22eec7fbb6f044735
Bart Zonneveld <bart.zonneveld@shopify.com>
2022-08-03 10:49:04 +0200

    Updated tmux status

---
+++ bin/tmux_status_right.rb
@@ -0,0 +1,12 @@
+parts = [
+  `~/.bin/battery_status.rb`,
+  `cat /tmp/next_calendar_meeting_parsed`,
+  `osascript ~/.bin/get_track.applescript`,
+  `TZ='Europe/Amsterdam' date +'%a, %e, %b %Y - %H:%M'`
+]
+
+puts parts
+  .map(&:chomp)
+  .reject(&:empty?)
+  .compact
+  .join("  |  ")
