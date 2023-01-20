ebc476b229322fa59ae8ae27b7c3ff6ee4a09230
Bart Zonneveld <bart.zonneveld@shopify.com>
2023-01-20 14:30:27 +0100

    Added dev aliases

---
+++ zsh/functions/dev_aliases.zsh
@@ -0,0 +1,29 @@
+local pwd="${PWD/#$HOME/~}"
+pwd_list=(${(s:/:)pwd})
+
+alias dt="dev test"
+alias dr="dev style"
+alias ds="./bin/srb typecheck"
+
+alias da="clear; ds && dr && dt"
+
+case $pwd_list[-1] in
+  "shopify")
+    alias dt="dev test --include-branch-commits"
+    alias dr="dev style --include-branch-commits"
+    ;;
+  "shipify")
+    alias dt="dev test --include-branch-commits"
+    alias dr="dev style --include-branch-commits"
+    alias ds="./bin/style typecheck"
+    ;;
+  "atlas")
+    alias dt="dev test"
+    alias dr="dev style"
+    ;;
+  *)
+    unalias dt
+    unalias dr
+    unalias ds
+    unalias da
+esac
