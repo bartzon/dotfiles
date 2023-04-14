9ba91257878216aaac0664497b4515bf8af01cd9
Bart Zonneveld <bart.zonneveld@shopify.com>
2023-04-14 08:31:38 +0200

    Using lsd instead of exa

--- linux/install.sh
+++ linux/install.sh
@@ -12,6 +12,6 @@ if ! command -v ripgrep &> /dev/null; then
   sudo apt-get install -y ripgrep
 fi
 
-if ! command -v diff-so-fancy &> /dev/null; then
-  npm i -g diff-so-fancy
+if ! command -v lsd &> /dev/null; then
+  sudo apt-get install -y lsd
 fi
