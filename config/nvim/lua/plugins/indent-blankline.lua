206ccb07dc3da7fb99cb94468440e722a7714dc1
Bart Zonneveld <bart.zonneveld@shopify.com>
2023-10-17 10:15:54 +0200

    Updates

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -1,15 +1,10 @@
-local Plugin = {'lukas-reineke/indent-blankline.nvim'}
+local Plugin = { 'lukas-reineke/indent-blankline.nvim' }
 
-Plugin.name = 'indent_blankline'
+Plugin.main = 'ibl'
 
-Plugin.opts = {
-  enabled = true,
-  char = '▏',
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = true,
-}
+function Plugin.config()
+  require('ibl').setup({
+  })
+end
 
 return Plugin
