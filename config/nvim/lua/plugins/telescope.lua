7e8d6a23d96c86152f45ca0c45d12f149ddb1342
Bart Zonneveld <bart.zonneveld@shopify.com>
2023-12-04 08:55:04 +0100

    Slimmed telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -2,45 +2,29 @@ local Plugin = { 'nvim-telescope/telescope.nvim' }
 
 Plugin.dependencies = {
   { 'nvim-lua/plenary.nvim' },
-  { 'dhruvmanila/browser-bookmarks.nvim' },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 }
 
 Plugin.cmd = 'Telescope'
 
 Plugin.opts = {
   defaults = {
+    find_command = { "fd", "-t=f", "-a" },
+    path_display = { "absolute" },
+    wrap_results = true,
     file_ignore_patterns = {
       'sorbet'
     },
-    defaults = {
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--hidden",
-        "--smart-case",
-      },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--hidden",
+      "--smart-case",
     },
   },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-    },
-    bookmarks = {
-      selected_browser = 'chrome',
-    }
-  }
 }
-
-function Plugin.init()
-  require('telescope').load_extension('bookmarks')
-  require('telescope').load_extension('fzf')
-end
 
 return Plugin
