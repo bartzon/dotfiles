668e654d6a95dee97ba7f38285085e2dc5699843
Bart Zonneveld <bart.zonneveld@shopify.com>
2024-01-03 13:42:41 +0100

    LSP updates

--- config/nvim/lua/plugins/lsp_config.lua
+++ config/nvim/lua/plugins/lsp_config.lua
@@ -17,49 +17,76 @@ Plugin.dependencies = {
   { 'jose-elias-alvarez/typescript.nvim' },
 }
 
+-- textDocument/diagnostic support until 0.10.0 is released
+local _timers = {}
+local function setup_diagnostics(client, buffer)
+  if require("vim.lsp.diagnostic")._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      local diagnostic_items = {}
+      if result then
+        diagnostic_items = result.items
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        nil,
+        vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+        { client_id = client.id }
+      )
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
+
 function Plugin.config()
   local lsp = require('lsp-zero')
 
-  lsp.ensure_installed({
-    'sorbet',
-    'ruby-lsp',
-    'rubocop',
-    'tsserver',
-    'kotlin',
-  })
-
-  lsp.on_attach(function(_, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
-  end)
-
   require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-  require('typescript').setup({
-    disable_commands = false, -- prevent the plugin from creating Vim commands
-    debug = false,            -- enable debug logging for commands
-    go_to_source_definition = {
-      fallback = true,        -- fall back to standard LSP definition on failure
-    },
-  })
-
+  require('typescript').setup {}
   require('lspconfig').sorbet.setup {}
-  require('lspconfig').rubocop.setup {}
   require('lspconfig').kotlin.setup {}
+  require('lspconfig').ruby_ls.setup({
+    on_attach = function(client, buffer)
+      setup_diagnostics(client, buffer)
+    end,
+  })
 
   lsp.setup()
 end
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = "none",
-  focusable = "false"
 })
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = "none",
-  focusable = "false"
 })
 
 vim.diagnostic.config({
-  virtual_text = true
+  virtual_text = true,
+  focusable = false,
 })
 
-return Plugin
+return {}
