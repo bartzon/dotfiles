95d5ece08b3d012951211c812fb29940456f9431
Bart Zonneveld <bart.zonneveld@shopify.com>
2025-09-12 09:59:43 +0200

    Updates

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -17,16 +17,33 @@ local token =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiNGIxOGQxNzQ5MTg0ZmQ1OTU5OGNhMTlhMTYwYWViNCIsImlhdCI6MTcyOTU5NTEyMywiZXhwIjoyMDQ0OTU1MTIzfQ.NEuuCSeCjnF0gvx4NvBSyrTFsAAOFwFvnVKsjSsXQ78"
 local headers = {}
 headers["Authorization"] = "Bearer " .. token
+headers["Content-Type"] = "application/json"
 local meeting_url = "http://homeassistant.local:8123/api/states/input_boolean.bart_in_meeting"
 
-local my_camera = hs.camera.allCameras()[1]
+-- Get available cameras with error handling
+local cameras = hs.camera.allCameras()
+if #cameras == 0 then
+  hs.alert.show("No cameras found!")
+  return
+end
+
+local my_camera = cameras[1]
+print("Using camera: " .. (my_camera:name() or "Unknown"))
+
 my_camera:setPropertyWatcherCallback(function(camera)
-  local in_meeting = ""
-  if (camera:isInUse()) then
-    in_meeting = "true"
-  else
-    in_meeting = "false"
-  end
-  hs.http.post(meeting_url, '{"state":' .. in_meeting .. '}', headers)
+  local in_use = camera:isInUse()
+  local state_value = in_use and "on" or "off"
+  local json_body = '{"state":"' .. state_value .. '"}'
+  
+  print("Camera in use: " .. tostring(in_use) .. ", sending: " .. json_body)
+  
+  hs.http.asyncPost(meeting_url, json_body, headers, function(status, body, headers)
+    if status == 200 then
+      print("Successfully updated meeting status to: " .. state_value)
+    else
+      print("Failed to update meeting status. Status: " .. tostring(status) .. ", Body: " .. tostring(body))
+    end
+  end)
 end)
+
 my_camera:startPropertyWatcher()
